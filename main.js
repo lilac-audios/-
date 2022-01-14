d06a5b8686ba1d59db6e32b0472c5c088027e64a
lilac <97724923+lilac-audios@users.noreply.github.com>
2022-01-14 12:50:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function edit_audioscp(){
+window.location.replace("edit_audios.html");
+}
+function project_files_cp(){
+    window.location.replace("project_files.html");
+    }
+
+function tutorials_cp(){
+    window.location.replace("tutorials.html");
+}
+
+    function home_page()
+    {
+        window.location.replace("index.html");
+    }
